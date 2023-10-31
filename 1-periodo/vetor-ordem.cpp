7260c7b8d085075d07cd8ba2146ba92132a1b013
Gabriela Farias <143662278+gabrielafariaas@users.noreply.github.com>
2023-10-30 21:20:23 -0300

    ordenando um vetor

--- 1-periodo/vetor-ordem.cpp
+++ 1-periodo/vetor-ordem.cpp
@@ -1,29 +1,41 @@
 #include <iostream>
+
 using namespace std;
-void crescente(int a[], int b) {
-  bool f = false;
-  for (int i = 0; i < b - 1; i++) {
-    if (a[i] < a[i + 1]) {
-      f = true;
-    } else {
-      f = false;
-      cout << "não está em ordem crescente ";
-      break;
+void b(int vetor[], int tamanho)
+{
+  for (int i = 0; i < tamanho; i++)
+  {
+
+    for (int j = 0; j < tamanho - i - 1; j++)
+    {
+      if (vetor[j] > vetor[j + 1])
+      {
+        int temp = vetor[j];
+        vetor[j] = vetor[j + 1];
+        vetor[j + 1] = temp;
+      }
     }
-  }
-  if (f) {
-    cout << "está em ordem crescente ";
   }
 }
 int main() {
-  int a[] = {1, 9, 3, 4, 5, 6, 7}, tam;
-  tam = sizeof(a) / sizeof(a[0]);
-  crescente(a, tam);
-  cout << " \n";
-  cout << "os elementos do vetor são: ";
-  for (int i : a) {
+  int vet[] = {4, 1, 3, 2, 0, 7};
+  int tam = sizeof(vet) / sizeof(vet[0]);
+
+  cout<<"vetor original = { ";
+  for (int i : vet) {
+
     cout << i << " ";
   }
+  cout<<"}";
+  cout << "\n";
 
+  b(vet, tam);
+  cout<<"vetor ordenado = { ";
+  for (int i : vet) {
+
+    cout << i << " ";
+  }
+  cout<<"}";
+  cout << "\n";
   return 0;
 }
